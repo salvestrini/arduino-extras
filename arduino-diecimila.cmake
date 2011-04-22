f14708fd9c10a5e5e67a6746660f241b0157e305
Francesco Salvestrini <salvestrini@gmail.com>
2011-04-22 22:54:51 +0200

    Added initial files

---
+++ arduino-diecimila.cmake
@@ -0,0 +1,4 @@
+message(STATUS "Including ${CMAKE_CURRENT_LIST_FILE}")
+
+set(ARDUINO_MCU  "-mmcu=atmega168")
+set(ARDUINO_DEFS "-DF_CPU=16000000")
---
+++ arduino-duemilanove.cmake
@@ -0,0 +1,4 @@
+message(STATUS "Including ${CMAKE_CURRENT_LIST_FILE}")
+
+set(ARDUINO_MCU  "-mmcu=atmega328p")
+set(ARDUINO_DEFS "-DF_CPU=16000000")
